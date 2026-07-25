5e211e9ba88a6ee3436ae31efbee50ac99ced3c7
abod724 <abdullaha0569361@gmail.com>
2026-07-26 02:01:25 +0300

    Refactor Config class to use os.environ
    Removed dotenv loading and SECRET_KEY from Config class.

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-    BING_API_KEY = os.getenv("BING_API_KEY", "")
-    DB_PATH = os.getenv("DB_PATH", "myais.db")
+    OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+    BING_API_KEY = os.environ.get("BING_API_KEY", "")
