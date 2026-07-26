edc775db0156aa7dbd42ee6aab81892856f5238c
abod724 <abdullaha0569361@gmail.com>
2026-07-26 09:54:44 +0300

    Add SQLAlchemy model for chat history

--- database.py
+++ database.py
@@ -1,2 +1,17 @@
-def init_db():
-    pass
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class ChatHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_message = db.Column(db.Text, nullable=False)
+    bot_response = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'user': self.user_message,
+            'bot': self.bot_response,
+            'time': self.timestamp.strftime('%Y-%m-%d %H:%M')
+        }
