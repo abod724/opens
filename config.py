678961782950621b27a115c180f8e5889633ef31
abod724 <abdullaha0569361@gmail.com>
2026-07-31 01:20:08 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # تحميل المتغيرات من ملف .env (اختياري)
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my-secret-key')
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///niras.db')
